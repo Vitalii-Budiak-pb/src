9fd309ef854426cacc2514a0b43b03b5bc66040d
Vitalii Budiak <vitalij.budjak@privatbank.ua>
2021-10-12 23:26:11 +0300

    commit Calculator Interval

---
+++ src/com/pb/budiak/hw2/Calculator.java
@@ -0,0 +1,43 @@
+package com.pb.budiak.hw2;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int operand1;
+        int operand2;
+        String sign;
+
+        System.out.print("Введите первое число: ");
+        operand1 = in.nextInt();
+        System.out.print("Введите второе число: ");
+        operand2 = in.nextInt();
+        System.out.print("Выберите знак арифметической операции (+,-,*,/): ");
+        sign = in.next();
+
+
+        switch (sign){
+            case "+":
+                System.out.println("Результат:" + (operand1 + operand2));
+                break;
+
+            case "-":
+                System.out.println("Результат:" + (operand1 - operand2));
+                break;
+
+            case "*":
+                System.out.println("Результат:" + (operand1 * operand2));
+
+            case "/":
+                if(operand2 != 0 ){
+                    System.out.println("Результат: " + (operand1 / operand2));
+                } else {
+                    System.out.println("Ошибка, деление на \"0\".\nПопробуйте заново!");
+                }
+        }
+
+
+    }
+}
---
+++ src/com/pb/budiak/hw2/Interval.java
@@ -0,0 +1,34 @@
+package com.pb.budiak.hw2;
+
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x;
+
+        System.out.print("Введите целое число в диапазоне от [0 - 100]: ");
+        x = in.nextInt();
+
+        if (x >= 0 && x <= 14){
+            System.out.println("Ваше число попадает в диапазон [0 - 14]");
+        }
+        else if (x < 0){
+            System.out.println("Ошибка!\nВаше число меньше \"0\"");
+        }
+
+        if (x >= 15 && x <= 35){
+            System.out.println("Ваше число попадает в диапазон [15 - 35]");
+        }
+
+        if (x >= 36 && x <= 50){
+            System.out.println("Ваше число попадает в диапазон [36 - 50]");
+        }
+
+        if (x >= 51 && x <= 100){
+            System.out.println("Ваше число попадает в диапазон [51 - 100]");
+        } else if (x > 100){
+            System.out.println("Ваше число больше допустимого диапазона.\nПопробуйте заново!");
+        }
+    }
+}
