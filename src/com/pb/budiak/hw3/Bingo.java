62364d175f06fa5852ea673cec57c23a2741e278
Vitalii Budiak <vitalij.budjak@privatbank.ua>
2021-10-21 22:27:42 +0300

    commit Bingo

--- src/com/pb/budiak/hw3/Bingo.java
+++ src/com/pb/budiak/hw3/Bingo.java
@@ -15,41 +15,35 @@ public class Bingo {
         int x = random.nextInt(101);
         System.out.println("Игра началась! \nЯ загадал целое число, давай ты попробуешь его отгадать? \nЯ лишь буду немного подсказывать!");
         System.out.print("Если ты готов, отправь мне подтверждение в виде \"Да\",\"Нет\": ");
+        statement = in.next();
+        while ("Да".equalsIgnoreCase(statement)) {
+            System.out.println("x = " + x);
+            System.out.print("Ваш ответ: ");
+            answer = in.nextInt();
+            if (answer == x) {
+                System.out.println("Поздравляю, Вы угадали число с " + attempt + "-й попытки!");
+                break;
+            } else {
+                if (answer > x) {
+                    System.out.println("Вы не угадали, но Ваше число больше загаданного мной!");
+                } else {
+                    System.out.println("Вы не угадали, но Ваше число меньше загаданного мной!");
+                }
+                System.out.print("Продолжаем? (\"Да\",\"Нет\") :");
                 statement = in.next();
-        while (statement.equals("Да")) {
-                System.out.println("x = " + x );
-                System.out.print("Ваш ответ: ");
-                answer = in.nextInt();
-                    if(answer == x){
-                        System.out.println("Поздравляю, Вы угадали число с " + attempt + "-й попытки!");
-                        break;
-                    }
-                    else {
-                        if(answer > x){
-                            System.out.println("Вы не угадали, но Ваше число больше загаданного мной!");
-                            System.out.print("Продолжаем? (\"Да\",\"Нет\") :");
-                            statement = in.next();
-                        }
-                        else {
-                            System.out.println("Вы не угадали, но Ваше число меньше загаданного мной!");
-                            System.out.print("Продолжаем? (\"Да\",\"Нет\") :");
-                            statement = in.next();
-                        }
-                        attempt++;
-                    }
+                attempt++;
+            }
+        }
 
-        }
-        if(statement.equals("Нет")){
-            System.out.println("Попробуй в следующий раз!");
-        }
-        else {
-            if(statement.equals("Да")){
+        switch (statement.toLowerCase()) {
+            case "нет":
+                System.out.println("Попробуй в следующий раз!");
+                break;
+            case "да":
                 System.out.println("\nИгра закончена!");
-            }
-            else {
+                break;
+            default:
                 System.out.println("Я не понимаю других команд, попробуй заново!\nИгра закончена!");
-            }
-
         }
     }
 }
